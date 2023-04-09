e2dd76c8e708e3800d133dc37539ae57329b96a5
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-04-09 14:18:17 -0300

    Added time info for the bot message

--- main/send_message.py
+++ main/send_message.py
@@ -1,10 +1,14 @@
 import os
 import requests
+from datetime import datetime
+import pytz
 
 dolar_scraper_token = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
 
 def send_message(buy, sell, chat_id):
-    msg = "COMPRA: $" + buy + "\n" + "VENTA: $" + sell
+    actual_time = datetime.now(tz=pytz.timezone("America/Argentina/Buenos_Aires"))
+    formatted_time = actual_time.strftime("%d/%m %H:%M")
+    msg = formatted_time + "\n" +"COMPRA: $" + buy + "\n" + "VENTA: $" + sell
     url = 'https://api.telegram.org/bot' + dolar_scraper_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + msg
 
     #response with message data
