711d1fada932f7b40a5e88cd3d69c47e2f21461f
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-04-09 19:07:33 -0300

    Added USDT price information to the message.

--- main/send_message.py
+++ main/send_message.py
@@ -1,14 +1,20 @@
 import os
 import requests
 from datetime import datetime
+from binance import get_p2p_ars_price
 import pytz
 
 dolar_scraper_token = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
 
 def send_message(buy, sell, chat_id):
+    #Gets formatted time info
     actual_time = datetime.now(tz=pytz.timezone("America/Argentina/Buenos_Aires"))
     formatted_time = actual_time.strftime("%d/%m %H:%M")
-    msg = formatted_time + "\n" +"COMPRA: $" + buy + "\n" + "VENTA: $" + sell
+
+    #Gets USDT - ARS price
+    usdt_ars = get_p2p_ars_price(mercado_pago=True)
+    
+    msg = formatted_time + "\n" + "Blue: $" + sell + "\n" + "USDT: $" + usdt_ars
     url = 'https://api.telegram.org/bot' + dolar_scraper_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + msg
 
     #response with message data
