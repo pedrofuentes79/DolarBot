02a48c6bb737971ae3579cb6ce2eb172ff970440
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-01 11:11:32 -0300

    Added AWS functionality

--- main/main.py
+++ main/main.py
@@ -1,15 +1,22 @@
-from scraper import get_blue
-from send_message import send_message
+from main.lib.get_blue import get_blue
+from main.lib.send_message import send_message
+from main.lib.get_p2p_ars_price import get_p2p_ars_price
 import os
+import json
 
-CHAT_IDS = [os.environ.get("TELEGRAM_CHAT_ID")]
-
-def main():
-    #gets buy and sell values
-    buy, sell = get_blue()
-    #sends the message to the chat ids
-    for chat_id in CHAT_IDS:
-        send_message(sell, chat_id)
-
-if __name__ == "__main__":
-    main()
+def lambda_handler(event, context):
+    CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+    DOLAR_SCRAPER_TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
+    
+    #Gets buy and sell values
+    _, blue = get_blue()
+    usdt = get_p2p_ars_price(mercado_pago=True)
+    
+    #Sends the message to the chat id
+    response, msg_id = send_message(usdt, blue, CHAT_ID, DOLAR_SCRAPER_TOKEN)
+    
+    print(response)    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
