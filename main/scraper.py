734c1559453ce0e8644f000b556242200f375474
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-01 11:19:02 -0300

    Reorganized files

--- main/scraper.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-def get_blue():
-    url = "https://www.dolarsi.com/api/api.php?type=dolar"
-    #gets dollar request in json format
-    r = get(url).json()
-    blue_buy = r[1]["casa"]["compra"]
-    blue_sell = r[1]["casa"]["venta"]
-    return blue_buy, blue_sell
