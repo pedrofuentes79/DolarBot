1edd5ae85d3331c7c5aa8f442136914019400fb1
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-11 15:00:27 -0300

    Fixed string format

--- main/lib/get_blue.py
+++ main/lib/get_blue.py
@@ -6,4 +6,9 @@ def get_blue():
     response = requests.get(url).json()
     blue_buy = response[1]["casa"]["compra"]
     blue_sell = response[1]["casa"]["venta"]
+    
+    #reformats the values so that they end with two decimals and use . instead of , for decimals
+    blue_buy = blue_buy[:-4] + ".00"
+    blue_sell = blue_sell[:-4] + ".00"
+    
     return blue_buy, blue_sell
