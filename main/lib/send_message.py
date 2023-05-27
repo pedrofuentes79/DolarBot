d9a4f10015dd436c32c167788e158a7aa8a6c31e
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-27 15:15:11 -0300

    Added emoji functionality

--- main/lib/send_message.py
+++ main/lib/send_message.py
@@ -1,15 +1,21 @@
 import requests
 import dateutil.tz
 from datetime import datetime
+from lib.last_price_utils import get_up_down_emoji
 
 
 
-def send_message(blue: str, usdt: str, chat_id: str, token: str):
-    #Gets formatted time info
+def send_message(blue: str, usdt: str, chat_id: str, token: str, date_str: str):
+    #Gets formatted time info for the message (could put this into the date_utils.py module)
     actual_time = datetime.now(tz=dateutil.tz.gettz("America/Argentina/Buenos_Aires"))
-    formatted_time = actual_time.strftime("%d/%m %H:%M")
+    message_formatted_time = actual_time.strftime("%d/%m %H:%M")
     
-    msg = formatted_time + "\n" + "Blue: $" + blue + "\n" + "USDT: $" + usdt
+    #Up/Down emoji
+    emoji = get_up_down_emoji(date_str=date_str
+                              current_price_blue=blue, 
+                              current_price_usdt=usdt)
+    
+    msg = message_formatted_time + "\n" + "Blue: $" + blue + "\n" + "USDT: $" + usdt
     url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + msg
 
     #response with message data
