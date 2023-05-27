d758b33642d7cc2b43c8b7adb52d86583ca25bdd
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-27 16:26:27 -0300

    Added get_last_price function

--- main/lib/last_price_utils.py
+++ main/lib/last_price_utils.py
@@ -1,26 +1,27 @@
 from lambda_invokers.ReadDynamoDB_invoker import ReadDynamoDB_invoker
 import json
 
+
 def get_up_down_emoji(date_str: str, current_price_usdt: int, current_price_blue: int):
     #gets last entries
-    last_entry_usdt = ReadDynamoDB_invoker(table_name="usdt_prices", date_str=date_str, limit=1)
     last_entry_blue = ReadDynamoDB_invoker(table_name="blue_prices", date_str=date_str, limit=1)
+    last_entry_usdt = ReadDynamoDB_invoker(table_name="usdt_prices", date_str=date_str, limit=1)
     
     #gets last prices
-    last_price_usdt_str = json.loads(last_entry_usdt["body"])["Items"][0]["price"]
     last_price_blue_str = json.loads(last_entry_blue["body"])["Items"][0]["price"]
+    last_price_usdt_str = json.loads(last_entry_usdt["body"])["Items"][0]["price"]
     
-    #makes them integers
-    last_price_usdt = int(last_price_usdt_str)
-    last_price_blue= int(last_price_blue_str)
+    #string to float
+    last_price_blue= float(last_price_blue_str)
+    last_price_usdt = float(last_price_usdt_str)
     
-    emoji_blue = ""
-    emoji_usdt = ""
+    emoji_blue: str
+    emoji_usdt: str
     
-    if current_price_blue > last_price_blue: emoji_blue = "up"
-    else:                                    emoji_blue = "down"
+    if current_price_blue > last_price_blue: emoji_blue = "📈"
+    else:                                    emoji_blue = "📉"
         
-    if current_price_usdt > last_price_usdt: emoji_usdt = "up"
-    else:                                    emoji_usdt = "down"
+    if current_price_usdt > last_price_usdt: emoji_usdt = "📈"
+    else:                                    emoji_usdt = "📉"
     
-    return emoji_usdt, emoji_blue 
+    return (emoji_blue, emoji_usdt)
