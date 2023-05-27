b504958d0c9e95058b1cb1263e1f7c4a2e63dc01
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-05-27 15:16:28 -0300

    Added comments

--- main/main.py
+++ main/main.py
@@ -10,6 +10,7 @@ import json
 
 
 def lambda_handler(event, context):
+    #Keys
     CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
     DOLAR_SCRAPER_TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
     
@@ -17,15 +18,17 @@ def lambda_handler(event, context):
     _, blue = get_blue()
     usdt = get_p2p_ars_price(mercado_pago=True)
     
+    #formatted date string
     date_str = get_formatted_date()
 
-
+    
     #stores the date, price and type of value in the DynamoDB tables respectively
     WriteDynamoDB_invoker(date=date_str, price=blue, type_of_value="sell", table_name="blue_prices"),
     WriteDynamoDB_invoker(date=date_str, price=usdt, type_of_value="sell", table_name="usdt_prices")
     
     #Sends the message to the chat id
-    response, msg_id = send_message(blue, usdt, CHAT_ID, DOLAR_SCRAPER_TOKEN)
+    response, msg_id = send_message(blue, usdt, CHAT_ID, DOLAR_SCRAPER_TOKEN, date_str
+)
     
     print(response)    
     return {
