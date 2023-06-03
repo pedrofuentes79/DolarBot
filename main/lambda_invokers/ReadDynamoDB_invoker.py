3bf3fbd1fd6b1b9ae605a3a9ffc1aecd8f486501
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-06-03 14:54:15 -0300

    Modified the function to use 'get_item' instead of 'query'

--- main/lambda_invokers/ReadDynamoDB_invoker.py
+++ main/lambda_invokers/ReadDynamoDB_invoker.py
@@ -6,27 +6,25 @@ from lib.date_utils import get_one_hour_less
 #i believe I will temporarily mantain the WriteDynamoDB_invoker but in the future it might be easier to do it all in the same function
 
 
-def ReadDynamoDB_invoker(table_name: str, date_str: str,limit: int=1):
+def ReadDynamoDB_invoker(table_name: str, date_str: str, limit: int=1):
+    '''
+    Given a date_str, this function returns the entry that corresponds
+    with the previous entry relative to that date_str
+    '''
     dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
     table_name = table_name
     
     table = dynamodb.Table(table_name)
     
-    
-    last_entry_date_str = get_one_hour_less(date_str)
+    last_entry_date = get_one_hour_less(date_str)
+    last_entry_date_int = int(last_entry_date.timestamp())
 
-    response = table.query(
-        KeyConditionExpression="#date = :date_val",
-        ExpressionAttributeNames={
-            "#date": "date"
+    response = table.get_item(
+        Key={
+            'unix_date': last_entry_date_int
         },
-        ExpressionAttributeValues={
-            ":date_val": date_str
-        },
-        ScanIndexForward=False,  # Sort in descending order
-        Limit=1
-    )
-
+        ProjectionExpression="price"
+        )
     
     return {
         'statusCode': 200,
