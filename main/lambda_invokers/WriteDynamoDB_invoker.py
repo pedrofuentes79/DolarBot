d6f2b07155f1cbb2613c7468f107735aee0cea2d
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-06-03 14:55:05 -0300

    Changed primary key to 'unix_date'

--- main/lambda_invokers/WriteDynamoDB_invoker.py
+++ main/lambda_invokers/WriteDynamoDB_invoker.py
@@ -1,14 +1,16 @@
 import boto3
 import json
+
     
-def WriteDynamoDB_invoker(date="", price=0, type_of_value="", table_name="blue_prices"):
+def WriteDynamoDB_invoker(date_int=1, date_str="" ,price=0, type_of_value="", table_name="blue_prices"):
     # defines the client
     client = boto3.client('lambda')
     
     # defines the structure of the payload to be sent 
     payload = json.dumps({
         "body": {
-            'date': date,
+            'unix_date': date_int,
+            'date_str': date_str,
             'price': price,
             'type_of_value': type_of_value
             },
