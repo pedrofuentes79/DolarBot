0a801bf8f253c3fa0928ad0a8a185ea5216879b8
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-06-03 15:00:19 -0300

    Changed send_message structure

--- main/main.py
+++ main/main.py
@@ -14,21 +14,19 @@ def lambda_handler(event, context):
     CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
     DOLAR_SCRAPER_TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
     
-    #Gets buy and sell values
+    #Gets sell values
     _, blue = get_blue()
     usdt = get_p2p_ars_price(mercado_pago=True)
     
     #formatted date string
-    date_str = get_formatted_date()
-
+    date_str, date_int = get_formatted_date()
     
-    #stores the date, price and type of value in the DynamoDB tables respectively
-    WriteDynamoDB_invoker(date=date_str, price=blue, type_of_value="sell", table_name="blue_prices"),
-    WriteDynamoDB_invoker(date=date_str, price=usdt, type_of_value="sell", table_name="usdt_prices")
+    #stores the date (as a timestamp int), date string (for legibility) ,price and type of value in the DynamoDB tables respectively
+    WriteDynamoDB_invoker(date_int=date_int, date_str=date_str, price=blue, type_of_value="sell", table_name="blue_prices"),
+    WriteDynamoDB_invoker(date_int=date_int, date_str=date_str, price=usdt, type_of_value="sell", table_name="usdt_prices")
     
     #Sends the message to the chat id
-    response, msg_id = send_message(blue, usdt, CHAT_ID, DOLAR_SCRAPER_TOKEN, date_str
-)
+    response, msg_id = send_message(blue, usdt, date_str, CHAT_ID, DOLAR_SCRAPER_TOKEN)
     
     print(response)    
     return {
