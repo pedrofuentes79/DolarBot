d154899ea297ffb06df4c042ff1e1ff3818e1953
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-06-25 16:08:12 -0300

    added more restrictions to sellers and calculates the mean of the last 5 sellers

--- main/lib/get_p2p_ars_price.py
+++ main/lib/get_p2p_ars_price.py
@@ -1,18 +1,39 @@
 import requests
+import numpy as np
 
 def get_p2p_ars_price(mercado_pago=True):
     '''
     Returns the lowest price for buying USDT with ARS and MercadoPago as a payment method
     through Binance's P2P.
+    Sellers must offer to sell at least $30.000 ARS and have at least 50 USDT available.
     '''
 
     url = "https://criptoya.com/api/binancep2p/buy/usdt/ars/1000000"
     response = requests.get(url).json()
     sellers = response["data"]
     
-    #for each seller, which are ordered from lowest to highest price, checks if they offer MercadoPago.
-    for i in range(len(sellers)):
-        price = sellers[i]["adv"]["price"]
-        for j in sellers[i]["adv"]["tradeMethods"]:
-            if mercado_pago and (j["identifier"] == "MercadoPagoNew"):
-                return price
+    if mercado_pago:
+        # Get the first 5 sellers that accept MercadoPago and offer to sell $30000 or more ARS
+
+        mp_sellers = []
+        for seller in sellers:
+            tradeMethods = seller["adv"]["tradeMethods"]
+            if len(mp_sellers) == 5:
+                    break
+            for method in tradeMethods:
+                if (method["tradeMethodName"] == "Mercadopago" and float(seller["adv"]["minSingleTransAmount"]) >= 25000 and float(seller["adv"]["surplusAmount"]) >= 50.0 ):
+                    #if conditions are met, add seller to list
+                    mp_sellers.append(seller)
+    else:
+        return sellers[0]["adv"]["price"]
+    
+    # Calculate average price on the mp_sellers list
+    prices = np.array([])
+    for seller in mp_sellers:
+        prices = np.append(prices, float(seller["adv"]["price"]))
+    
+    # Calculate mean price rounded to 2 decimals
+    mean_price = round(np.mean(prices), 2)
+    
+    return mean_price
+
