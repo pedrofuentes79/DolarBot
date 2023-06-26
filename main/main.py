25605abbd4cd444b911e347b276546c27bd61e74
pedrofuentes79 <pedrofuentes7799@gmail.com>
2023-06-26 17:17:04 -0300

    changed the order of two lines

--- main/main.py
+++ main/main.py
@@ -14,14 +14,16 @@ def lambda_handler(event, context):
     CHAT_ID = os.environ.get("TELEGRAM_CHANNEL_ID")
     DOLAR_SCRAPER_TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
     
-    #Gets sell values
-    _, blue = get_blue()
-    usdt = get_p2p_ars_price(mercado_pago=True)
+
     
     #formatted date string
     date_str, date_int = get_formatted_date()
     
-    #stores the date (as a timestamp int), date string (for legibility) ,price and type of value in the DynamoDB tables respectively
+    #Gets sell values
+    _, blue = get_blue()
+    usdt = get_p2p_ars_price(mercado_pago=True, date_str=date_str)
+    
+    #stores the date (as a timestamp int), date string (for legibility), price and type of value in the DynamoDB tables respectively
     WriteDynamoDB_invoker(date_int=date_int, date_str=date_str, price=blue, type_of_value="sell", table_name="blue_prices"),
     WriteDynamoDB_invoker(date_int=date_int, date_str=date_str, price=usdt, type_of_value="sell", table_name="usdt_prices")
     
