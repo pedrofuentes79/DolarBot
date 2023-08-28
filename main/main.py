4852a6223eb8def95356558e0b7c3adcf19c89eb
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-28 12:52:51 -0300

    added checks for friday last hour and end month last hour (to send the corresponding charts). Also added a datetime object instead of a date_str.

--- main/main.py
+++ main/main.py
@@ -1,12 +1,14 @@
 from lib.get_blue import get_blue
-from lib.date_utils import get_formatted_date, holiday
-from lib.send_message import send_message
+from lib.date_utils import get_formatted_date, holiday, is_friday_last_hour, is_end_month
+from lib.send_message import send_message, send_chart
 from lib.get_p2p_ars_price import get_p2p_ars_price
+from lib.chart import get_weekly_chart
 
 from lambda_invokers.WriteDynamoDB_invoker import WriteDynamoDB_invoker
 
 import os
 import json
+import datetime
 
 
 def lambda_handler(event, context):
@@ -18,6 +20,18 @@ def lambda_handler(event, context):
     
     #formatted date string
     date_str, date_int = get_formatted_date()
+
+    date = datetime.datetime.strptime(date_str, "%d.%m.%Y:%H.%M")
+
+    if is_friday_last_hour(date):
+        chart_buffer = get_weekly_chart()
+        response_chart = send_chart(chart_buffer, DOLAR_SCRAPER_TOKEN, CHAT_ID)
+        print(response_chart)
+        
+    if is_end_month(date):
+        # send monthly chart
+        pass
+
     
     if not holiday(date_str):
         #Gets sell values
