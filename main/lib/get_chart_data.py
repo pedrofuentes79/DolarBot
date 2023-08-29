c8d9940dad2e4858024b532843444453a833b925
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-29 13:12:41 -0300

    changed date_str implementation to date_dt for simplicity and less redundancy in the code altogether

--- main/lib/get_chart_data.py
+++ main/lib/get_chart_data.py
@@ -6,18 +6,15 @@ import pytz
 OPENING = 10
 CLOSING = 17
 
-def get_weekly_data(table_name, date_friday_str):
+def get_weekly_data(table_name, date_friday):
     dynamodb = boto3.resource('dynamodb', region_name="us-east-2")
     table = dynamodb.Table(table_name)
 
-    date_friday = dt.strptime(date_friday_str, "%d.%m.%Y:%H.%M")
     date_monday = date_friday.replace(day=date_friday.day-4, hour=OPENING, minute=0, second=0, microsecond=0)
     
-    date_friday_with_tz = date_friday.replace(tzinfo=pytz.timezone("America/Argentina/Buenos_Aires"))
-    date_monday_with_tz = date_monday.replace(tzinfo=pytz.timezone("America/Argentina/Buenos_Aires"))
-    
-    start_timestamp = int(date_monday_with_tz.timestamp())
-    end_timestamp = int(date_friday_with_tz.timestamp())
+    # get the timestamp with some seconds of margin
+    start_timestamp = int(date_monday.timestamp()) - 60
+    end_timestamp = int(date_friday.timestamp()) + 60
 
     
     response = table.scan(
@@ -31,21 +28,16 @@ def get_weekly_data(table_name, date_friday_str):
 
     return response['Items']
 
-def get_monthly_data(table_name, date_end_month_str):
+def get_monthly_data(table_name, date_end_month):
 
     dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
     table = dynamodb.Table(table_name)
 
-    date_end_month = dt.strptime(date_end_month_str, "%d.%m.%Y:%H.%M")
-    date_start_month = date_end_month.replace(day=1, hour=OPENING, minute=0, second=0, microsecond=0)
-    
-    # replace with local tz
-    date_end_month_with_tz = date_end_month.replace(tzinfo=pytz.timezone("America/Argentina/Buenos_Aires"))
-    date_start_month_with_tz = date_start_month.replace(tzinfo=pytz.timezone("America/Argentina/Buenos_Aires"))
+    date_start_month = date_end_month.replace(day=1, hour=OPENING, minute=0, second=0, microsecond=0, tzinfo=pytz.timezone("America/Argentina/Buenos_Aires"))
 
-    # convert to timestamp
-    start_timestamp = int(date_start_month_with_tz.timestamp())
-    end_timestamp = int(date_end_month_with_tz.timestamp())
+    # convert to timestamp with some seconds of margin
+    start_timestamp = int(date_start_month.timestamp()) - 60
+    end_timestamp = int(date_end_month.timestamp()) + 60
     
     # scan between start and end of the month.
     response = table.scan(
