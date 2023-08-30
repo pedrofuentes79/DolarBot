902455bb58df7a0d161a44328d24975454ccd27e
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-30 14:33:33 -0300

    improved graph

--- main/lib/chart.py
+++ main/lib/chart.py
@@ -1,37 +1,36 @@
 import matplotlib.pyplot as plt
 from io import BytesIO
-from datetime import datetime as dt
-from lib.get_chart_data import get_weekly_data, get_monthly_data
+import datetime
 from dateutil import tz
 from date_utils import get_month_spanish
 from constants import OPENING, CLOSING
 
 
 def get_weekly_chart(data):
-    # sort the data by timestamp
+    mytz = tz.gettz('America/Argentina/Buenos_Aires')
     data.sort(key=lambda x: x["unix_date"])
 
-    dates = [dt.fromtimestamp(x["unix_date"], tz=tz.gettz('America/Argentina/Buenos_Aires')) for x in data]
-    dates_formatted = [x.strftime("%A %H:%M") for x in dates]
-    prices = [float(x["price"]) for x in data]
+    prices = []
+    dates = []
+    for x in data:
+        curr_dt = datetime.datetime.fromtimestamp(x["unix_date"], tz=mytz)
+        # only one price per day
+        if curr_dt.hour == CLOSING:
+            prices.append(float(x["price"]))
+            dates.append(curr_dt)
     
-    # make the plot so that there are 5 ticks on the x axis
-    # these 5 ticks should be the opening and closing times of the last 5 days
-    x_ticks = []
-    for date in dates:
-        if date.hour == OPENING:
-            x_ticks.append(date.strftime("%A %H:%M"))
-    
-    # append the last date for friday's closing time
-    x_ticks.append(dates_formatted[-1])
+    dates_formatted = [date.strftime("%d") for date in dates]
 
-    plt.plot(dates_formatted, prices)
-    plt.xticks(x_ticks, rotation=45)
-    plt.xlabel("Date")
-    plt.ylabel("Price")
-    plt.title("Weekly chart")
+    plt.figure(figsize=(10, 6))
+
+    plt.fill_between(dates_formatted, prices, color="skyblue", alpha=0.4)
+    plt.plot(dates_formatted, prices, color="Slateblue", alpha=0.6, linewidth=2)
+    
+    plt.ylim(min(prices) * 0.9, max(prices) * 1.05)
+    plt.xlabel("Día")
+    plt.ylabel("Precio")
     plt.tight_layout()
-    plt.grid()
+    plt.grid(alpha=0.5)
     
     # save the plot in a buffer to be sent to telegram
     buffer = BytesIO()
@@ -42,24 +41,35 @@ def get_weekly_chart(data):
     return buffer    
 
 def get_monthly_chart(data):
-    # sort the data by timestamp
+    mytz = tz.gettz('America/Argentina/Buenos_Aires')
     data.sort(key=lambda x: x["unix_date"])
 
-    dates = [dt.fromtimestamp(x["unix_date"], tz=tz.gettz('America/Argentina/Buenos_Aires')) for x in data]
-    dates_formatted = [x.strftime("%d") for x in dates]
-    prices = [float(x["price"]) for x in data]
+    prices = []
+    dates = []
+    for x in data:
+        curr_dt = datetime.datetime.fromtimestamp(x["unix_date"], tz=mytz)
+        # only one price per day
+        if curr_dt.hour == CLOSING:
+            prices.append(float(x["price"]))
+            dates.append(curr_dt)
 
-    mes = get_month_spanish(dates[0])
+    mes = get_month_spanish(dates[0].month)
     
-    x_ticks = [date.strftime("%d") for date in dates if (date.hour == OPENING)]
+    dates_formatted = [date.strftime("%d") for date in dates]
+
+    plt.figure(figsize=(10, 6))
+
+    # Shades the are below the curve
+    plt.fill_between(dates_formatted, prices, color="skyblue", alpha=0.4)
+    # Plots the curve
+    plt.plot(dates_formatted, prices, color="Slateblue", alpha=0.6, linewidth=2)
     
-    plt.plot(dates_formatted, prices)
-    plt.xticks(x_ticks, rotation=45)
+    plt.ylim(min(prices) * 0.8, max(prices) * 1.05)
     plt.xlabel("Día")
-    plt.ylabel("Precio")
-    plt.title(f"Gráfico de {mes}")
+    plt.ylabel("Dólar Blue")
+    plt.title(f"Dólar Blue en {mes}")
     plt.tight_layout()
-    plt.grid()
+    plt.grid(alpha=0.5)
 
     # save the plot in a buffer to be sent to telegram
     buffer = BytesIO()
