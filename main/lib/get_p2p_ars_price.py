be260a6a46a87e3440d0300fa02d67c919e5e14a
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-31 14:35:58 -0300

    Improved mean calculation and added comments

--- main/lib/get_p2p_ars_price.py
+++ main/lib/get_p2p_ars_price.py
@@ -4,11 +4,8 @@ from lib.last_price_utils import get_previous_prices
 
 
 def get_p2p_ars_price(mercado_pago, date_dt):
-    '''
-    Returns the lowest price for buying USDT with ARS and MercadoPago as a payment method
-    through Binance's P2P.
-    Sellers must offer to sell at least $30.000 ARS and have at least 50 USDT available.
-    '''
+    # Returns the lowest price for buying USDT with ARS and MercadoPago as a payment method through Binance's P2P.
+    # Sellers must offer to sell at least $30.000 ARS and have at least 50 USDT available.
 
     url = "https://criptoya.com/api/binancep2p/buy/usdt/ars/1000000"
     response = requests.get(url).json()
@@ -23,24 +20,23 @@ def get_p2p_ars_price(mercado_pago, date_dt):
             if len(mp_sellers) == 5:
                     break
             for method in tradeMethods:
-                if (method["tradeMethodName"] == "Mercadopago" and float(seller["adv"]["minSingleTransAmount"]) >= 25000 and float(seller["adv"]["surplusAmount"]) >= 50.0 ):
-                    #if conditions are met, add seller to list
+                if (method["tradeMethodName"] == "Mercadopago"            and 
+                    float(seller["adv"]["minSingleTransAmount"]) >= 25000 and 
+                    float(seller["adv"]["surplusAmount"]) >= 50.0 ):
                     mp_sellers.append(seller)
     else:
         return sellers[0]["adv"]["price"]
     
-    if mp_sellers != []:
+    if mp_sellers:
         # Calculate average price on the mp_sellers list if its not empty
-        prices = np.array([])
-        for seller in mp_sellers:
-            prices = np.append(prices, float(seller["adv"]["price"]))
-        
+        prices = [float(seller["adv"]["price"]) for seller in mp_sellers]
+        prices = np.array(prices)
+
         # Calculate mean price rounded to 2 decimals
         mean_price = round(np.mean(prices), 2)
         
         return str(mean_price)
     else:
-        # If there are no sellers, repeat the previous price.
+        # If there are no valid sellers, repeat the previous price.
         _, last_price_usdt = get_previous_prices(date_dt)
         return last_price_usdt
-
