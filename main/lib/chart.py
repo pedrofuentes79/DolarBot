31ef5ed4799e4099a85f7f492487ea484318d0e8
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-08-31 14:34:29 -0300

    Added spanish weekdays to the weekly chart and added comments to the code

--- main/lib/chart.py
+++ main/lib/chart.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import datetime
 from dateutil import tz
-from date_utils import get_month_spanish
+from date_utils import get_month_spanish, weekday_to_spanish
 from constants import OPENING, CLOSING
 
 
@@ -14,25 +14,30 @@ def get_weekly_chart(data):
     dates = []
     for x in data:
         curr_dt = datetime.datetime.fromtimestamp(x["unix_date"], tz=mytz)
-        # only one price per day
+        # Get only one price per day
         if curr_dt.hour == CLOSING:
             prices.append(float(x["price"]))
             dates.append(curr_dt)
     
-    dates_formatted = [date.strftime("%d") for date in dates]
+    # Get the day of the week in spanish
+    dates_formatted = [weekday_to_spanish(date) for date in dates]
+
 
     plt.figure(figsize=(10, 6))
 
+    # Shades the area below the curve and plots the curve
     plt.fill_between(dates_formatted, prices, color="skyblue", alpha=0.4)
     plt.plot(dates_formatted, prices, color="Slateblue", alpha=0.6, linewidth=2)
     
     plt.ylim(min(prices) * 0.9, max(prices) * 1.05)
     plt.xlabel("Día")
-    plt.ylabel("Precio")
+    plt.ylabel("Dólar Blue")
+    # agregar entre qué días fue esta semana?
+    plt.title("Dólar Blue Semanal")
     plt.tight_layout()
     plt.grid(alpha=0.5)
     
-    # save the plot in a buffer to be sent to telegram
+    # Save the plot in a buffer to be sent to telegram
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
     buffer.seek(0)
@@ -59,9 +64,8 @@ def get_monthly_chart(data):
 
     plt.figure(figsize=(10, 6))
 
-    # Shades the are below the curve
+    # Shades the area below the curve and plots the curve
     plt.fill_between(dates_formatted, prices, color="skyblue", alpha=0.4)
-    # Plots the curve
     plt.plot(dates_formatted, prices, color="Slateblue", alpha=0.6, linewidth=2)
     
     plt.ylim(min(prices) * 0.8, max(prices) * 1.05)
@@ -71,7 +75,7 @@ def get_monthly_chart(data):
     plt.tight_layout()
     plt.grid(alpha=0.5)
 
-    # save the plot in a buffer to be sent to telegram
+    # Save the plot in a buffer to be sent to telegram
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
     buffer.seek(0)
