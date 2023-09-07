74982950accd9615edc6a71b9ffae32524f3ebe2
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-09-07 10:50:33 -0300

    added send_current_prices

--- webhook/send_message.py
+++ webhook/send_message.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime
 
 def send_message(message, chat_id):
     TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
@@ -16,4 +17,15 @@ def send_message_prices(date_str, opening, closing, chat_id):
 
     url = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + chat_id + '&parse_mode=HTML&text=' + message
     response = requests.get(url)
+    return response
+
+def send_current_prices(blue, usdt, chat_id):
+    TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
+    
+    date_str = datetime.datetime.now().strftime("%d/%m:%H:%M")
+    
+    message = date_str + "\n" + "Blue: $" + blue +"\n" + "USDT: $" + usdt
+    
+    url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + chat_id + "&parse_mode=Markdown&text=" + message
+    response = requests.get(url).json()
     return response
