0e76934060624720cce12ff995e664632178db90
Pedro Fuentes <pedrofuentes7799@gmail.com>
2023-09-08 22:05:20 -0300

    added tz to the current date

--- webhook/send_message.py
+++ webhook/send_message.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import datetime
+from dateutil import tz
 
 def send_message(message, chat_id):
     TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
@@ -22,7 +23,7 @@ def send_message_prices(date_str, opening, closing, chat_id):
 def send_current_prices(blue, usdt, chat_id):
     TOKEN = os.environ.get("TELEGRAM_API_DOLAR_SCRAPER_TOKEN")
     
-    date_str = datetime.datetime.now().strftime("%d/%m:%H:%M")
+    date_str = datetime.datetime.now(tz=tz.gettz("America/Argentina/Buenos_Aires")).strftime("%d/%m:%H:%M")
     
     message = date_str + "\n" + "Blue: $" + blue +"\n" + "USDT: $" + usdt
     
